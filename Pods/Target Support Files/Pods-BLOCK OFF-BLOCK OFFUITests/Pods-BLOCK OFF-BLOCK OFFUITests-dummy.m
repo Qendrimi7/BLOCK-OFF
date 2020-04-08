29003a6e6e5916daae1fbf2ec73c3e2b650bfc9a
Qëndrim Mjeku <qendrimmjeku@gmail.com>
2020-04-09 00:08:26 +0200

    add image app icon and install pod file empty

---
+++ Pods/Target Support Files/Pods-BLOCK OFF-BLOCK OFFUITests/Pods-BLOCK OFF-BLOCK OFFUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BLOCK_OFF_BLOCK_OFFUITests : NSObject
+@end
+@implementation PodsDummy_Pods_BLOCK_OFF_BLOCK_OFFUITests
+@end
---
+++ Pods/Target Support Files/Pods-BLOCK OFF/Pods-BLOCK OFF-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_BLOCK_OFF : NSObject
+@end
+@implementation PodsDummy_Pods_BLOCK_OFF
+@end
